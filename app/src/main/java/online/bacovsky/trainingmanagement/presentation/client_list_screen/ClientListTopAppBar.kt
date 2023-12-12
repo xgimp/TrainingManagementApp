775ffbf609e070b13df8c318441596781d1b252b
Lukas Bacovsky <l.bacovsky@gmail.com>
2023-12-12 15:38:44 +0100

    added filter dropdown menu

--- app/src/main/java/online/bacovsky/trainingmanagement/presentation/client_list_screen/ClientListTopAppBar.kt
+++ app/src/main/java/online/bacovsky/trainingmanagement/presentation/client_list_screen/ClientListTopAppBar.kt
@@ -1,19 +1,27 @@
 package online.bacovsky.trainingmanagement.presentation.client_list_screen
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.FilterList
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.res.stringResource
 import online.bacovsky.trainingmanagement.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ClientListTopAppBar() {
+    val isMenuExpanded = rememberSaveable { mutableStateOf(false) }
+
     TopAppBar(
         title = {
             Text(
@@ -22,11 +30,42 @@ fun ClientListTopAppBar() {
             )
         },
         actions = {
-            IconButton(
-                onClick = {/* TODO */}
-            ) {
-                Icon(Icons.Outlined.FilterList, contentDescription = "Filter List")
+            Box {
+                IconButton(
+                    onClick = {
+                        isMenuExpanded.value = !isMenuExpanded.value
+                    }
+                ) {
+                    Icon(Icons.Outlined.FilterList, contentDescription = "Filter List")
+                    ClientFilterMenu(isExpanded = isMenuExpanded)
+                }
             }
         }
     )
+}
+
+
+@Composable
+fun ClientFilterMenu(
+    isExpanded: MutableState<Boolean>
+) {
+    DropdownMenu(
+        expanded = isExpanded.value,
+        onDismissRequest = {
+            isExpanded.value = false
+        }
+    ) {
+        DropdownMenuItem(
+            text = {
+                   Text(text = "Item 1")
+            },
+            onClick = { isExpanded.value = false }
+        )
+        DropdownMenuItem(
+            text = {
+                   Text(text = "Item 2")
+            },
+            onClick = { /*TODO*/ }
+        )
+    }
 }
