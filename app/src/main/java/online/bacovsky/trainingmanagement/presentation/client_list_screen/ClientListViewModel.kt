2a8e9c521ec59384837f45a2fcb7dee195c49abf
Lukáš Bačovský <l.bacovsky@gmail.com>
2023-12-16 12:22:31 +0100

    unused import

--- app/src/main/java/online/bacovsky/trainingmanagement/presentation/client_list_screen/ClientListViewModel.kt
+++ app/src/main/java/online/bacovsky/trainingmanagement/presentation/client_list_screen/ClientListViewModel.kt
@@ -2,7 +2,6 @@ package online.bacovsky.trainingmanagement.presentation.client_list_screen
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
