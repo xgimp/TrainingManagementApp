07cfd0e3d8278f5637019c4033199bdf4c542214
Lukas Bacovsky <l.bacovsky@gmail.com>
2024-01-07 17:05:23 +0100

    unused imports

--- app/src/main/java/online/bacovsky/trainingmanagement/presentation/sms_screen/ProgressBar.kt
+++ app/src/main/java/online/bacovsky/trainingmanagement/presentation/sms_screen/ProgressBar.kt
@@ -2,20 +2,11 @@ package online.bacovsky.trainingmanagement.presentation.sms_screen
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
